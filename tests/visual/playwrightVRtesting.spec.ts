a5595c8937abfdb711e72ae9b71b26e504c61d99
Chukwuma Anyadike <chukwuma.anyadike@gmail.com>
2026-06-14 10:21:14 -0500

    Addition of visual regression testing

---
+++ tests/visual/playwrightVRtesting.spec.ts
@@ -0,0 +1,33 @@
+import { expect, Locator, test } from '@playwright/test';
+import { generateFullURL, playwright_dev_locators } from '../../main/configuratons/config';
+
+test.describe('Test playwright locators page with visual regression tesing', () => {
+
+    const playwrightLocatorsURL = generateFullURL(playwright_dev_locators);
+
+    test.beforeEach(async ({ page }) => {
+        await page.goto(playwrightLocatorsURL);
+    })
+
+    test('visual regression testing of playwright locators page', async ({ page }) => {
+
+        // allow page to load
+        await page.waitForURL(/locators/);
+
+        // compare page screenshot to baseline image
+        await expect(page).toHaveScreenshot();
+
+    });
+
+    test('visual regression testing of an element', async ({ page }) => {
+
+        // allow element to load
+        const playwrightLogo: Locator = page.getByAltText("playwright logo").first();
+        await playwrightLogo.isVisible();
+
+        // compare element screenshot to baseline image
+        await expect(playwrightLogo).toHaveScreenshot();
+
+    });
+
+});
